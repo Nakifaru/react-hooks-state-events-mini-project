af29e4f80dc0f683cca57973074c9ea999815261
Michael <michaelmwendwamumo@gmail.com>
2024-02-08 11:56:32 +0300

    List of tasks displayed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ TASKS }) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {/* display a list of tasks using Task component */
+      TASKS.map( function(task) {
+        return <Task task={task} key={task.text}/>
+      })
+      }
     </div>
   );
 }
